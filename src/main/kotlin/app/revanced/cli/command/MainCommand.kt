29b6e4e7196769ec15dbc3ee077cf04a529c8f01
inotia00 <108592928+inotia00@users.noreply.github.com>
2024-12-07 11:03:51 +0900

    feat: add command `options` (`options.json` generator)

--- src/main/kotlin/app/revanced/cli/command/MainCommand.kt
+++ src/main/kotlin/app/revanced/cli/command/MainCommand.kt
@@ -35,6 +35,7 @@ private object CLIVersionProvider : IVersionProvider {
     subcommands = [
         PatchCommand::class,
         JsonCommand::class,
+        OptionsCommand::class,
         ListPatchesCommand::class,
         ListCompatibleVersions::class,
         UtilityCommand::class,
